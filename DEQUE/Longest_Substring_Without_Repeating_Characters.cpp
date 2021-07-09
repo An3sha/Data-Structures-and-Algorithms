036ccdf122d320e756562438f034cfaa25576dfa
ANISHA SINGH <anishasingh439@gmail.com>
2021-07-09 16:16:32 +0530

    Add files via upload

---
+++ DEQUE/Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,63 @@
+/* 
+
+Using MAP   : MOST OPTIMISED SOLUTION
+
+Time complexity : O(n)
+Space complexity : O(1) */
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        vector<int> mpp(256, -1); 			// because we have 256possible characters, we can also use vector<int> mpp(char, int);
+        int maxSize=0, i=0, j=0, n=s.size();
+        
+        while(j<n){
+            
+            if(mpp[s[j]] != -1) 
+            {
+            	i = max(mpp[s[j]]+1, i);
+            }
+            
+            mpp[s[j]] = j;
+            
+            maxSize = max(maxSize, j-i+1);
+            
+            j++;
+           
+        }
+        return maxSize;
+    }
+};
+
+
+/*
+
+Using SET
+
+Time complexity : O(2n)   			:a character will be visited twice, once by r pointer (in O(n)time) and once by l pointer (in O(n)time) 
+Space complexity : O(1)   */
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        set<char> st;
+        int maxSize=0, i=0, j=0, n=s.size();
+        while(j<n)
+        {
+            if(st.count(s[j])==0)
+            {
+                st.insert(s[j]);
+                maxSize = max(maxSize, j-i+1);
+                j++;
+            }
+            else
+            {
+                st.erase(s[i]);
+                i++;
+            }
+        }
+        return maxSize;
+    }
+};
