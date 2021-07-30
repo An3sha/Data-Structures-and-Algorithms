2ccc5e2efef19f74cfbd0032fb4a4c856b534c28
ANISHA SINGH <anishasingh439@gmail.com>
2021-07-30 13:12:27 +0530

    Update Fibonacci.cpp

--- DYNAMIC PROGRAMMING/Fibonacci.cpp
+++ DYNAMIC PROGRAMMING/Fibonacci.cpp
@@ -1,3 +1,63 @@
+//General Solution using recursion
+
+#include<iostream>
+using namespace std;
+
+int fib(int n)
+{
+	if(n==0)
+		return 0;
+	if(n==1)
+		return 1;
+	
+	int ans = fib(n-1) + fib(n-2);
+	return ans;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int ans = fib(n);
+	cout<<n<<th fib no. is<<ans; 		//if we try to find the 40th fib no. it'll take years(in terms of computer) to calculate the value.
+}
+/* Time Complexity : O(2^n)			
+
+				 
+
+								     f(5) 		       ------------------------------------------ 2^0			
+								  /	  \
+							   	 /	   \
+							        /	    \
+							       /	     \
+							      /		      \
+							     /		       \
+							    /		        \
+							   /			 \
+						  	  /       		  \
+						        f(4)			  f(3)	        ------------------------------------------- 2^1	
+						        / \			  / \
+						       /   \			 /   \
+						      /	    \			/     \
+						     /	     \ 		       /       \
+					         f(3)       f(2)            f(2)       f(1)     ------------------------------------------- 2^2
+					         / \         / \	     / \        
+					        /   \       /   \           /   \      
+				               f(2)  f(1)  f(1) f(0)      f(1) f(0)  		------------------------------------------- 2^3
+				               /  \
+					      /    \
+					    f(1)  f(0)						------------------------------------------- 2^4
+
+
+
+				 Time Complexity: 2^0 + 2^1 + 2^2 + 2^3 + 2^4  = 2^n
+				 Time Complexity: 2^n
+
+*/
+
+
+//OPTIMIZED SOLUTION using MEMOIZATION or DIVIDE & CONQUER 
+
 #include <iostream>
 using namespace std;
 
