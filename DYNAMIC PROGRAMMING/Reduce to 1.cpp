4a993cd9970c32b40b00c030da58d82257db6c01
ANISHA SINGH <anishasingh439@gmail.com>
2021-07-31 17:17:04 +0530

    Update Reduce to 1.cpp

--- DYNAMIC PROGRAMMING/Reduce to 1.cpp
+++ DYNAMIC PROGRAMMING/Reduce to 1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorith>   //for fill algo
 using namespace std;
 const int inf = 1e9;
 
@@ -24,8 +25,31 @@ int reduce_dp(int n)
   return dp[n];
 }
   
+//MEMOIZATION 
+
+int memo[10000];
+int reduce(int n)
+{
+  if(n==1)
+    return 0;
   
+  int q1=inf, q2=inf, q3=inf;
   
+  if(memo[n]!= -1)
+  {
+    return memo[n];     //because this has already been computed
+  }
+  
+  if(n%3==0)
+    q1 = 1+reduce(n/3);
+   if(n%2==0)
+    q2 = 1+reduce(n/2);
+  q3 = 1+reduce(n-1);
+  
+  memo[n] = min(q1, min(q2, q3));     //memoize have to be done
+  return memo[n];
+}
+
 
 /* GENERAL SOLUTION USING RECURSION
 
@@ -44,12 +68,18 @@ int reduce(int n)
   return min(q1, min(q2, q3));
 }
 */
+
+
 int main ()
 {
   int n;
   cin>>n;
-  int ans = reduce(n);
+ // int ans = reduce(n);
+  
+  //fill(memo, memo+n+1, -1);        //for(memo=-1; memo<n+1; memo++)
+  //int ans = reduce(n);
+  
   int ans = reduce_dp(n);
-  int ans = memodp(n);
+  
   cout<<ans;
 }
