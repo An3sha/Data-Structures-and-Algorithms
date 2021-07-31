dcc84887acc8de72ba8859706046d29a27484734
ANISHA SINGH <anishasingh439@gmail.com>
2021-07-31 18:29:56 +0530

    Update Fibonacci.cpp

--- DYNAMIC PROGRAMMING/Fibonacci.cpp
+++ DYNAMIC PROGRAMMING/Fibonacci.cpp
@@ -11,11 +11,11 @@ int fib(int n)
 	int i;
 	a[0]=0;
 	a[1]=1;
-	for(i=2; i<n; i++)
+	for(i=2; i<=n; i++)
 	{
 		a[i] = a[i-1] + a[i-2];
 	}
-	return a[i-1];
+	return a[n];
 }
 
 Time Complexity : O(n)
