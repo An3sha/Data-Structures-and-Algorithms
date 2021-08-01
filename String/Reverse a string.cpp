d98db80f684adc61ab79d38dec9fa6ab2a827e27
ANISHA SINGH <anishasingh439@gmail.com>
2021-08-01 18:37:43 +0530

    Add files via upload

---
+++ String/Reverse a string.cpp
@@ -0,0 +1,18 @@
+/* Input: s = ["h","e","l","l","o"]
+   Output: ["o","l","l","e","h"]  */
+
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int i=0;
+        int j=n-1;
+        while(i<j)
+        {
+            swap(s[i++],s[j--]);
+        }
+        
+    }
+};
