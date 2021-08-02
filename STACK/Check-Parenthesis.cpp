6d5c15a7d6c1482ccc9ba2c47f95c4a89192a5f8
ANISHA SINGH <anishasingh439@gmail.com>
2021-08-02 22:27:54 +0530

    Update Check-Parenthesis.cpp

--- STACK/Check-Parenthesis.cpp
+++ STACK/Check-Parenthesis.cpp
@@ -1,3 +1,5 @@
+/* return true if it's a valid parenthesis */
+
 #include <iostream>
 #include <stack>
 
@@ -71,5 +73,41 @@ int main()
  	// Time Complexity : O(n)
  	// Space Complexity : O(n)
 	
+/* Valid Parenthesis String
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
 
+The following rules define a valid string:
 
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "". */
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> asterisk;
+        stack<int> validPar;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(') validPar.push(i);
+            if(s[i] == '*') asterisk.push(i);
+            if(s[i] == ')'){
+                if(!validPar.empty()) 
+                    validPar.pop();
+                else if(!asterisk.empty()) 
+                    asterisk.pop();
+                else 
+                    return false;
+            }
+        }
+        
+        while(!validPar.empty() && !asterisk.empty()){
+            if(validPar.top() > asterisk.top()) return false;
+            
+            validPar.pop(); asterisk.pop();
+        }
+        
+        return validPar.empty();
+    }
+};
