2bf186d17ab1199da3e19821c02df8b00fee499c
ANISHA SINGH <anishasingh439@gmail.com>
2021-08-04 22:42:58 +0530

    Create General ques.cpp

---
+++ General ques.cpp
@@ -0,0 +1,27 @@
+/* BINARY TO DECIMAL
+
+input = 101
+output = 5
+
+*/
+
+int binaryToDecimal(int n)
+{
+    int num = n;
+    int dec_value = 0;
+ 
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+ 
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+ 
+        dec_value += last_digit * base;
+ 
+        base = base * 2;
+    }
+    return dec_value;
+}
+ 
