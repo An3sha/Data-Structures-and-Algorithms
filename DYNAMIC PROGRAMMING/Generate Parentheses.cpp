c9761acd4d3818dfc5c4a9ce63adafa1afc030c0
ANISHA SINGH <anishasingh439@gmail.com>
2021-08-14 22:20:49 +0530

    Update Generate Parentheses.cpp

--- DYNAMIC PROGRAMMING/Generate Parentheses.cpp
+++ DYNAMIC PROGRAMMING/Generate Parentheses.cpp
@@ -1,4 +1,4 @@
-vector<string> generateParenthesis(int n)
+int generateParenthesis(int n)
 {
   int dp[n+1];
   dp[0] = 0;
@@ -19,7 +19,24 @@ vector<string> generateParenthesis(int n)
   return dp[n];
 }
 
-
+// if want ()())()) output
+class Solution {
+public:
+    vector<string> ans;
+    void f(string s,int open,int close){        //open => '(' count remaining
+        if(open==0&&close==0){                  //close=> ')' count remaining
+            ans.push_back(s);
+            return;
+        }
+        if(open>0)f(s+"(",open-1,close);
+        if(open<close)f(s+")",open,close-1);    //'(' must be placed before ')'
+    }
+    vector<string> generateParenthesis(int n) {
+        ans.clear();
+        f("",n,n);                              //Balanced string will have
+        return ans;                             //n-open and n-closing brackets
+    }
+};
 
 
 
