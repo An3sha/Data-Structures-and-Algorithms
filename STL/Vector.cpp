133bc405f8c5619d6e780bb7732e460dba48de96
ANISHA SINGH <anishasingh439@gmail.com>
2021-09-19 20:14:09 +0530

    Update Vector.cpp

--- STL/Vector.cpp
+++ STL/Vector.cpp
@@ -33,5 +33,23 @@ int main()
   //Capacity of vactor
   cout << arr.capacity() << endl;
   
+  
+  
+  // 2D Vector
+  vector<vector<int>> arr = { {1,2,3},
+                              {4,5,6},
+                              {7,8,9,10},
+                              {11,12}};
+  arr[0][0] += 10;    //update
+  
+  for(int i=0; i<arr.size(); i++)
+  {
+    for(int number : arr[i])
+    {
+      cout << number << ",";
+    }
+    cout << endl;
+  }
+  
   return 0;
 }
