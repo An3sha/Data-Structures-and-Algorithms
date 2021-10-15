d6f0c8a059012d5fd956b56d48739e963d7fbdfe
ANISHA SINGH <anishasingh439@gmail.com>
2021-10-15 17:57:13 +0530

    Update 3Sum.cpp

--- Leetcode/3Sum.cpp
+++ Leetcode/3Sum.cpp
@@ -1,23 +1,81 @@
-#include<iostream>
-#define ll long long int
-using namespace std;
+/* using Two Pointers approach
+	Time : O(N*N)
+	Space : O(M). */
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& num) {
+        vector<vector<int>> res; 
+        sort(num.begin(), num.end()); 
+        
+        // moves for a
+        for (int i = 0; i < (int)(num.size())-2; i++) {
+            
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                
+                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
+                
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        
+                        vector<int> temp; 
+                        temp.push_back(num[i]); 
+                        temp.push_back(num[lo]); 
+                        temp.push_back(num[hi]); 
+                        res.push_back(temp);
+                        
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        
+                        lo++; hi--;
+                    } 
+                    else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+               }
+            }
+        }
+        return res;
+    }
+};
 
-int main()
-{
-	int t;
-	cin >> t;
-	while(t--)
-	{
-		ll n, s;
-		cin >> n >> s;
-		ll sum = (n*(n+1))/2;
-		if(s <= sum)
-		{
-			cout << -1 << endl;
-		}
-		else
-		{
-			cout << sum-s << endl;
-		}
-	}
-}
+
+
+
+/* Time : O(N^2 * logM)
+   Space : O(M) + O(N).
+   M for set */
+
+//using hashing
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());     //Sorted Array
+        if(nums.size() < 3){                // Base Case 1
+            return {};
+        }
+        if(nums[0] > 0){                    // Base Case 2
+            return {};
+        }
+        unordered_map<int , int> hashMap;
+        for(int i = 0 ; i < nums.size() ; ++i){     //Hashing of Indices
+            hashMap[nums[i]] = i;
+        }
+        vector<vector<int>> answer;
+        for(int i = 0 ; i < nums.size() - 2 ; ++i){     //Traversing the array to fix the number.
+            if(nums[i] > 0){     //If number fixed is +ve, stop there because we can't make it zero by searching after it.
+                break;
+            }
+            for(int j = i + 1 ; j < nums.size() - 1 ; ++j){     //Fixing another number after first number
+                int required = -1*(nums[i] + nums[j]);    //To make sum 0, we would require the -ve sum of both fixed numbers.
+                if(hashMap.count(required) && hashMap.find(required)->second > j){ //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+                    answer.push_back({nums[i] , nums[j] , required});
+                }
+                j = hashMap.find(nums[j])->second; //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+            }
+            i = hashMap.find(nums[i])->second;     //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+        }
+        return answer;  //Return answer vector.
+    }
+};
+
+
+
