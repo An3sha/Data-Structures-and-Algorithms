5f079834a7bc629bd3f137b1c45572cccb70a288
ANISHA SINGH <anishasingh439@gmail.com>
2021-10-25 23:02:08 +0530

    Create Space20.cpp

---
+++ Level-Up-course/String Poblems/Space20.cpp
@@ -0,0 +1,9 @@
+/* Given a string, write a function to replace all spaces in a string with %20. It is given that the string has sufficient space at the end to hold the additional characters.
+
+Sample Input : hello world,  how are you?
+Sample output : hello%20world,%20%20how%20are%20you?
+
+
+count the spaces first
+
+*/
