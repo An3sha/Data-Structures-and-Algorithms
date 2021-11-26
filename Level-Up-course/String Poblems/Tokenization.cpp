9ee93d3f51af561c22b28c63d85acf6b4a3cb14d
ANISHA SINGH <anishasingh439@gmail.com>
2021-11-27 00:56:54 +0530

    Create Tokenization.cpp

---
+++ Level-Up-course/String Poblems/Tokenization.cpp
@@ -0,0 +1,78 @@
+/* Tokenization denotes splitting a string with respect to some delimiter
+
+input: "today is a sunny day"
+output: "today", "is", "a", "sunny", "day"
+
+String tokenization in C++ :
+  1. using stringstream class
+  2. using strtok function
+  
+  
+              STRINGSTREAM
+              ____________
+*/
+
+
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+  string input;
+  getline(cin,input);     //when we have to take input inside a stringn we use getline function
+  
+  //create a string stream object
+  stringstream ss(input);   //initializing ss object with same as input string
+  
+  // >> and << operators which helps us to read and write data
+  string token;
+  vector<string> tokens;
+  while(getline(ss,token,' '))        //we are reading here and put inside tokens
+  {
+    tokens.push_back(token);
+  }
+  
+  //print all tokens
+  for(auto token:tokens)
+  {
+    cout<<token<<",";
+  }
+  
+  return 0;
+}
+
+//                                  STRTOK                              it works on character arrays
+                                    ______
+             
+                                      
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+  
+  char input[10000];
+  cin.getline(input,10000);
+  
+  //strtok()
+  cout<<strtok(input," ");      //it gives one word only
+  
+  
+  
+  
+  return 0;
+}
+
+                                     
+
+
+
+
+
+
+
