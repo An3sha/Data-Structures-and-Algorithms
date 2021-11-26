afeda0be0ba7a4c86197e4e0a53611b381e23a13
ANISHA SINGH <anishasingh439@gmail.com>
2021-11-26 21:56:12 +0530

    Update Space20.cpp

--- Level-Up-course/String Poblems/Space20.cpp
+++ Level-Up-course/String Poblems/Space20.cpp
@@ -8,3 +8,40 @@ Sample output : hello%20world,%20%20how%20are%20you?
 count the spaces first
 
 */
+
+
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void replace_space(char *str)
+{
+  // 1. Calc spaces
+  int spaces = 0;
+  for(int i=0; str[i]!='\0'; i++)
+  {
+    if(str[i]==' ')
+    {
+      spaces += 1;
+    }
+  }
+  int index = strlen(str) + 2*spaces;
+  str[index] = '\0';
+  
+  
+  
+  
+  
+}
+
+
+int main()
+{
+  char input[10000];
+  cin.getline(input,10000);
+  replace_space(input);
+  cout<<input<<endl;
+  
+  return 0;
+}
