88d44eb05dd23ea83c3180b82f1c8c72be52d662
ANISHA SINGH <anishasingh439@gmail.com>
2022-10-25 18:51:43 +0530

    Update search, insert, and delete in an unsorted array.cpp

--- ARRAY/search, insert, and delete in an unsorted array.cpp
+++ ARRAY/search, insert, and delete in an unsorted array.cpp
@@ -11,11 +11,22 @@ int searchkey(int arr[], int n, int key)    //searching Time:O(n); Space:0(1)
     else
       return -1;
 }
+  
+int insersorted(int arr[], int n, int N, int p)     //inserting p
+{
+  if(N >= n)
+  
+    return N;
+  
+  arr[N]==p;
+  return (N+1);
+}
 
 int main()
 {
   int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int n = sizeof(arr)/sizeof(arr[0]);
+  int N = 10;
   
   int pos = searchkey(arr, n, key);
   
@@ -29,6 +40,14 @@ int main()
   {
     cout<<"element found at position:"<<pos+1;
   }
+  
+  int p = 11;
+  
+  N = insertsorted(arr, n, N, p);
+  
+  cout << "\n After Insertion: ";
+  for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
   
   return 0;
   
