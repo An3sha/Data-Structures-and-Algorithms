0b26e01e90fc4258c1f262485765101b22460b7b
ANISHA SINGH <anishasingh439@gmail.com>
2023-01-03 18:14:17 +0530

    Create Subarray and subset.cpp

---
+++ ARRAY/Subarray and subset.cpp
@@ -0,0 +1,36 @@
+/*
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]. */
+
+class Solution {
+  public:
+  vactor<vector<int>> subsets(vactor<int> &nums)
+  {
+    int n = nums.size();
+    vector<vector<int>> ans;
+    int count = pow(2, n);
+    
+    for(int i=0; i<count; i++)
+    {
+      vector<int> temp;
+      for(int j=0; j<n; j++)
+      {
+        if(i & (1<<j))
+        {
+          temp.push_back(nums[j]);
+        }
+      }
+      
+      ans.push_back(temp);
+    }
+    
+    return ans;
+  }
+};
+
+/* 
+Time Complexity = O(2^n*n)
+Space Complexity = O(1)
+*/
+  
+  
