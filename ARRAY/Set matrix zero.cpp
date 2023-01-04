1cfab26952ba3c4a0c13b97fc604cafde699efea
ANISHA SINGH <anishasingh439@gmail.com>
2023-01-04 20:18:55 +0530

    Update Set matrix zero.cpp

--- ARRAY/Set matrix zero.cpp
+++ ARRAY/Set matrix zero.cpp
@@ -2,15 +2,17 @@
 
 /* Assuming all the elements in the matrix are non-negative
 
-Time Complexity:O((N*M)*(N + M)). O(N*M)
+Time Complexity:O((N*M)*(N + M))
 Space Complexity:O(1)
 */
 
 
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int row=matrix.size(), col=matrix[0].size();
+    void setZeroes(vector<vector<int>>& matrix) {           // since it is passed by referrence (&) we don't need to return anything. The matrix itself will change
+        
+        int row=matrix.size(), col=matrix[0].size();        // for col, we take matrix[0] that is any row of the matrix and calculate its size which will eventually give the matrix's column size
+        
         for(int i=0; i<row; i++)
         {
             for(int j=0; j<col; j++)
@@ -86,7 +88,7 @@ public:
 
 Time Complexity: O(N*M + N*M)
 
-Space Complexity: O(N)
+Space Complexity: O(M)+O(N)
 
  */
 
@@ -122,3 +124,59 @@ public:
         
     }
 };
+
+
+
+/* Optimizing the better approach
+
+Instead of taking two dummy arrays we can use the first row and column of the matrix for the same work. This will help to reduce the space complexity of the problem. While traversing for the second time
+the first row and column will be computed first, which will affect the values of further elements that’s why we traversing in the reverse direction.
+
+Time Complexity: O(2*(N*M)), as we are traversing two times in a matrix
+
+Space Complexity: O(1)
+
+*/
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+  for (int i = 0; i < rows; i++) {
+    //checking if 0 is present in the 0th column or not
+    if (matrix[i][0] == 0) col0 = 0;
+    for (int j = 1; j < cols; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+  //traversing in the reverse direction and
+  //checking if the row or col has 0 or not
+  //and setting values of matrix accordingly.
+  for (int i = rows - 1; i >= 0; i--) {
+    for (int j = cols - 1; j >= 1; j--) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
+    }
+    if (col0 == 0) {
+      matrix[i][0] = 0;
+    }
+
+  }
+
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
