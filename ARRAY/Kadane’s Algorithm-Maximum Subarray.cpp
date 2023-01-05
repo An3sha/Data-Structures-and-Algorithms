e0618bce525fddea3a6b3cfd9d34deb5d69da640
ANISHA SINGH <anishasingh439@gmail.com>
2023-01-05 17:46:26 +0530

    Update Kadane’s Algorithm-Maximum Subarray.cpp

--- ARRAY/Kadane’s Algorithm-Maximum Subarray.cpp
+++ ARRAY/Kadane’s Algorithm-Maximum Subarray.cpp
@@ -108,3 +108,17 @@ public:
 
 
 /* TRY USING DP */
+
+public int maxSubArray(int[] A) {
+        int n = A.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = A[0];
+        int max = dp[0];
+        
+        for(int i = 1; i < n; i++){
+            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+}
