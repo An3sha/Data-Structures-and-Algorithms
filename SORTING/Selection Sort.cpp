dad5b0755156b42ac97d4ee153f7c25a1fc7376d
ANISHA SINGH <anishasingh439@gmail.com>
2023-05-19 18:53:31 +0530

    Add files via upload

---
+++ SORTING/Selection Sort.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n) {
+
+
+	for ( int i = 0; i < n - 1; i++)
+
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[min] > arr[j]) {
+				min = j;
+			}
+		}
+
+		int temp = arr[min];
+		arr[min] = arr[i];
+		arr[i] = temp;
+	}
+
+	cout << "After selection sort: " << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int arr[] = {13, 46, 24, 52, 20, 9};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Before selection sort: " << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+	selection_sort(arr, n);
+	return 0;
+}
+
+
+
+//Time Complexity : O(N^2) best, worst, avg
+//Space Complexity: O(1)
